a3149ce938f6dfd98bbe09900ea6ee0313f662d3
Sébastien Bénard <fork@deepnight.net>
2019-12-16 12:25:18 +0100

    Disabled base options

--- src/Options.hx
+++ src/Options.hx
@@ -3,8 +3,8 @@ class Options {
 	public var flashes = false;
 	public var camShakes = false;
 	public var controlLocks = false;
-	public var basicAnimations = true;
-	public var gunAiming = true;
+	public var basicAnimations = false;
+	public var gunAiming = false;
 
 	public function new() {
 	}
