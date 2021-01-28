8d71c60df5ed9e0548175969532cd956f35e86e2
Sébastien Bénard <github@deepnight.net>
2021-01-28 14:41:27 +0100

    Update intro text

--- src/ui/Intro.hx
+++ src/ui/Intro.hx
@@ -14,11 +14,9 @@ class Intro extends ui.Modal {
 		var t = new h2d.Text(Assets.fontSmall, win);
 		t.textColor = 0x0;
 		t.maxWidth = 250;
-		t.text = "This prototype is not exactly an actual game. It was developed to "+
-			"serve as a demonstration for a \"Game feel\" talk.\n\n"+
-			"It shows the impact of small details on the overall quality of a game.\n\n"+
-			"You will need a GAMEPAD to test it. You can enable or disable game "+
-			"features in this demo by pressing the START button.\n\n"+
+		t.text = "This prototype is not exactly an actual game.\n\n"+
+			"It shows the impact of small details on the overall final quality and 'game feel'.\n\n"+
+			"To toggle game features: press gamepad START, or keyboard ENTER.\n\n"+
 			"Press any key to continue.";
 
 		dn.Process.resizeAll();
